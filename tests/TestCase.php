ee7cc7fcbcb05f4cdd1c9ef6d5d3532e8946acc9
Dexter Harrison <dexter.harrison@lapsafe.com>
2023-06-01 16:41:04 +0100

    Set Testing Models

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,8 @@ namespace LapSafe\ExtensionAttributes\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use LapSafe\ExtensionAttributes\ExtensionAttributesServiceProvider;
+use LapSafe\ExtensionAttributes\Models\ExtensionAttribute;
+use LapSafe\ExtensionAttributes\Tests\TestModels\User;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
@@ -27,6 +29,11 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
+
+        config()->set('extension-attributes.models', [
+            'user' => User::class,
+            'extension_attribute' => ExtensionAttribute::class
+        ]);
 
         $migration = include __DIR__.'/../database/migrations/create_extension_attributes_table.php.stub';
         $migration->up();
